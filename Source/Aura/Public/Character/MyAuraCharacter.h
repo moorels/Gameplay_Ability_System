8265801ede23ac2af34ab2d93a83c0cb4b6a5708
moorels <sales@micropls.com>
2023-10-22 15:51:52 +1100

    The Base Character Class1

---
+++ Source/Aura/Public/Character/MyAuraCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "MyAuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AMyAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
